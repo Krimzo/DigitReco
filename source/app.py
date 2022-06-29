b39f5dd407221d5022c75a55ea23d9bd9ccfba1f
Crimson17 <46162167+Krimzo@users.noreply.github.com>
2022-06-29 17:20:20 +0200

    Update app.py

--- source/app.py
+++ source/app.py
@@ -3,7 +3,7 @@ from util import *
 
 st.title("Digital Number Recognition")
 
-image_file = st.file_uploader(type=["png", "jpg", "jpeg", "bmp"], accept_multiple_files=False)
+image_file = st.file_uploader("", type=["png", "jpg", "jpeg", "bmp"], accept_multiple_files=False)
 
 if image_file is not None:
     image = load_image(image_file)
