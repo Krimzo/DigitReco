7ee4c528f3a20ccdeeced2f0f336ca61e5c6eee1
Crimson17 <46162167+Krimzo@users.noreply.github.com>
2022-06-29 22:29:49 +0200

    image preprocess

--- source/util.py
+++ source/util.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pytesseract as ts
+import numpy as np
+import cv2 as cv
 from PIL import Image
 
 
@@ -20,5 +22,9 @@ def show_image(img):
 
 
 def read_digits(img):
-    raw_digits = ts.image_to_string(img, config="--psm 12 --oem 3")
+    edited_image = img.convert('L')
+    ign, edited_image = cv.threshold(np.array(edited_image), 125, 255, cv.THRESH_BINARY)
+    edited_image = Image.fromarray(edited_image.astype(np.uint8))
+
+    raw_digits = ts.image_to_string(edited_image)
     return "".join(c for c in raw_digits if c in "0123456789,.")
