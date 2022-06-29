1612b6c21ab74e3548c40f2b8454bef0e6a0ebe9
Crimson17 <46162167+Krimzo@users.noreply.github.com>
2022-06-29 17:50:36 +0200

    button fix

--- source/app.py
+++ source/app.py
@@ -8,6 +8,6 @@ image_info = st.file_uploader("", type=["png", "jpg", "jpeg", "bmp"], accept_mul
 if image_info is not None:
     image_file = load_image(image_info)
     if image_file is not None:
-        if st.button("Read Digits", help="Reads the image file and looks for digits"):
+        if centered_button("Read Digits", "Reads the image file and looks for digits"):
             centered_text("Digits: " + read_digits(image_file))
             write_image(image_file)
--- source/util.py
+++ source/util.py
@@ -14,6 +14,22 @@ def centered_text(text):
     st.markdown("<h2 style='text-align: center; color: white;'>" + text + "</h2>", unsafe_allow_html=True)
 
 
+def centered_button(text, help_text):
+    button_state = False
+    col1, col2, col3 , col4, col5 = st.beta_columns(5)
+    with col1:
+        pass
+    with col2:
+        pass
+    with col4:
+        pass
+    with col5:
+        pass
+    with col3:
+        button_state = st.button(text, help=help_text)
+    return button_state
+
+
 def load_image(image_file):
 	img = Image.open(image_file)
 	return img
